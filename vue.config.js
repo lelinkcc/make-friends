f8ce4a80841781058de23bd6c1501e63ca14c9fc
wyywyy <1759365366@qq.com>
2023-06-14 16:44:00 +0800

    提交

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '': {       //'/api'是自行设置的请求前缀
-        target:"http://192.168.1.119:8001",
+        target:"http://testapi.lelink.cc",
         pathRewrite: { '^/api': '' },  //路径重写，（正则）匹配以api开头的路径为空（将请求前缀删除）
         changeOrigin: true ,
       },
